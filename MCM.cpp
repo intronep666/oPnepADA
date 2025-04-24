aeff3c8936b195cd475443eed1b2345866b4e15a
intronep666 <113291826+intronep666@users.noreply.github.com>
2025-04-24 23:36:45 +0530

    Update MCM.cpp

--- MCM.cpp
+++ MCM.cpp
@@ -1,121 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-//function to print split matrices
-void print(vector<vector<int>>&s,int i,int j)
-{
-    if(i==j) 
-    {
-        cout<<"A"<<i;
-    }
-    else
-    {
-        cout<<"(";
-        print(s,i,s[i][j]);
-        print(s,s[i][j]+1,j);
-        cout<<")";
+// Function to display the optimal order of matrix multiplication
+void displayOrder(const vector<vector<int>> &split, int start, int end) {
+    if (start == end) {
+        cout << "M" << start + 1;
+    } else {
+        cout << "(";
+        displayOrder(split, start, split[start][end]);
+        displayOrder(split, split[start][end] + 1, end);
+        cout << ")";
     }
 }
 
-// Function to find the minimum cost of matrix chain multiplication
-void matrixchain(vector<int>&p)
-{
-   int n = p.size() - 1; // number of matrices
-   vector<vector<int>>m(n,vector<int>(n,0)); //cost matrix 
-   vector<vector<int>>s(n,vector<int>(n,0));//split matrix
-   for(int l =2;l<=n;l++)
-   {
-    for(int i = 0;i<n-l+1;i++)
-    {
-        int j = i+l-1;
-        m[i][j] = INT_MAX;
-        for(int k =i;k<=j-1;k++)
-        {
-           int q = m[i][k] + m[k+1][j] + p[i]*p[k+1]*p[j+1];
-           if(q<m[i][j]){
-            m[i][j] = q;
-            s[i][j] = k;
-           }
+// Function to compute the minimum multiplication cost
+void optimalMatrixChain(const vector<int> &dims) {
+    int matrices = dims.size() - 1;
+    vector<vector<int>> cost(matrices, vector<int>(matrices, 0));
+    vector<vector<int>> splitIndex(matrices, vector<int>(matrices, 0));
+
+    // l is the chain length
+    for (int len = 2; len <= matrices; ++len) {
+        for (int i = 0; i <= matrices - len; ++i) {
+            int j = i + len - 1;
+            cost[i][j] = INT_MAX;
+
+            for (int k = i; k < j; ++k) {
+                int currCost = cost[i][k] + cost[k + 1][j] + dims[i] * dims[k + 1] * dims[j + 1];
+                if (currCost < cost[i][j]) {
+                    cost[i][j] = currCost;
+                    splitIndex[i][j] = k;
+                }
+            }
         }
     }
-   }
-   cout << "Minimum cost: " << m[0][n - 1] << endl;
-     print(s,0,n-1);
+
+    cout << "Minimum number of scalar multiplications: " << cost[0][matrices - 1] << endl;
+    cout << "Optimal parenthesization: ";
+    displayOrder(splitIndex, 0, matrices - 1);
+    cout << endl;
 }
-int main()
-{
-    vector<int> p = {5,10,15,20,25,30};
-    matrixchain(p);
+
+int main() {
+    vector<int> dimensions = {5, 10, 15, 20, 25, 30};
+    optimalMatrixChain(dimensions);
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Function to find the minimum cost of matrix chain multiplication
-// void matrixchain(vector<int> &p) {
-//     int n = p.size() - 1; // Number of matrices
-//     vector<vector<int>> m(n, vector<int>(n, 0)); // Cost matrix
-//     vector<vector<int>> s(n, vector<int>(n, 0)); // Split matrix
-
-//     // Fill the cost matrix using dynamic programming
-//     for (int l = 2; l <= n; l++) { // l is the chain length
-//         for (int i = 0; i < n - l + 1; i++) {
-//             int j = i + l - 1;
-//             m[i][j] = INT_MAX;
-//             for (int k = i; k < j; k++) {
-//                 int q = m[i][k] + m[k + 1][j] + p[i] * p[k + 1] * p[j + 1];
-//                 if (q < m[i][j]) {
-//                     m[i][j] = q;
-//                     s[i][j] = k;
-//                 }
-//             }
-//         }
-//     }
-
-//     cout << "Minimum cost: " << m[0][n - 1] << endl;
-//     cout << "Split points: " << endl;
-//     for (int i = 0; i < n - 1; i++) {
-//         cout << s[i][i + 1] << " ";
-//     }
-//     cout << endl;
-// }
-// int main()
-// {
-//     vector<int>p = {5,10,20,2,1};
-//     matrixchain(p);
-//     return 0;
-
-// }
