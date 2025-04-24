7b523e28acfe1935b3ece6904167afe2465dc04e
intronep666 <113291826+intronep666@users.noreply.github.com>
2025-04-25 00:24:08 +0530

    Update magicsquare.cpp

--- magicsquare.cpp
+++ magicsquare.cpp
@@ -1,46 +1,47 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <iomanip>
 using namespace std;
 
-void magicsquare(vector<vector<int>> &a, int n) {
-    int i = 0, j = n / 2; // Start at the middle of the first row
+// Function to construct an odd-order magic square
+void generateMagicSquare(vector<vector<int>>& grid, int size) {
+    int row = 0, col = size / 2;
 
-    for (int k = 1; k <= n * n; k++) {
-        a[i][j] = k; // Place the number in the grid
+    for (int num = 1; num <= size * size; num++) {
+        grid[row][col] = num;
 
-        // Store current position before moving
-        i = (i - 1 + n) % n; // Move up (wrap around using modulo)
-        j = (j + 1) % n;     // Move right (wrap around using modulo)
+        int nextRow = (row - 1 + size) % size;
+        int nextCol = (col + 1) % size;
 
-        // If the new cell is already occupied, move down instead
-        if (a[i][j] != 0) {
-            i = (i + 1) % n; // Move down
-        
+        // If cell is already filled, move down from current row
+        if (grid[nextRow][nextCol] != 0) {
+            row = (row + 1) % size;
+        } else {
+            row = nextRow;
+            col = nextCol;
         }
-    } 
+    }
 }
 
 int main() {
-    int n;
-    cin >> n;
+    int order;
+    cout << "Enter an odd number for the order of the magic square: ";
+    cin >> order;
 
-    if (n % 2 == 0) {
-        cout << "Magic square is only possible for odd numbers." << endl;
-        return 1;
+    if (order % 2 == 0) {
+        cout << "Only odd-sized magic squares are supported.\n";
+        return 0;
     }
 
-    // Creating a 2D vector initialized with 0
-    vector<vector<int>> a(n, vector<int>(n, 0));
+    vector<vector<int>> matrix(order, vector<int>(order, 0));
+    generateMagicSquare(matrix, order);
 
-    // Generate magic square
-    magicsquare(a, n);
-
-    // Display the magic square
-    cout << "Magic Square of order " << n << ":\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << setw(4) << a[i][j]; // Print formatted numbers
+    cout << "\nGenerated Magic Square of size " << order << ":\n";
+    for (const auto& row : matrix) {
+        for (int value : row) {
+            cout << setw(4) << value;
         }
-        cout << endl;
+        cout << '\n';
     }
 
     return 0;
