71c10a7dd58c199eea2f09967d535670c9fa2dc7
intronep666 <113291826+intronep666@users.noreply.github.com>
2025-04-24 23:59:42 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,49 +1,49 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
 using namespace std;
 
-void bubbleSort(long int* arr[], int n) 
-{
-        
-        for(int i=0;i<n-1;i++)
-        {
-            for(int j=0;j<n-1-i;j++)
-            {
-                if(arr[j]>arr[j+1]){
-                    swap(arr[j],arr[j+1]);
-                }
+void performBubbleSort(long int* data, int length) {
+    for (int pass = 0; pass < length - 1; ++pass) {
+        for (int idx = 0; idx < length - pass - 1; ++idx) {
+            if (data[idx] > data[idx + 1]) {
+                swap(data[idx], data[idx + 1]);
             }
         }
- }
+    }
+}
 
 int main() {
-    srand(time(0)); // Seed for random numbers
+    srand(static_cast<unsigned int>(time(nullptr))); // Initialize random seed
 
-    int n = 1000;
-    while (n <= 25000) {
-        double totalTime = 0.0;
+    int size = 1000;
+    while (size <= 25000) {
+        double accumulatedTime = 0.0;
 
-        for (int i = 1; i <= 10; i++) {
-            long int arr[n];
-            
-            // Generate random array of size n
-            for (int j = 0; j < n; j++) {
-                arr[j] = rand() % n + 1;
+        for (int trial = 0; trial < 10; ++trial) {
+            long int* values = new long int[size];
+
+            // Fill array with random integers
+            for (int i = 0; i < size; ++i) {
+                values[i] = rand() % size + 1;
             }
 
-            auto start = chrono::high_resolution_clock::now();
-            bubbleSort(arr, n);
-            auto end = chrono::high_resolution_clock::now();
+            auto begin = chrono::steady_clock::now();
+            performBubbleSort(values, size);
+            auto finish = chrono::steady_clock::now();
 
-            chrono::duration<double> duration = end - start;
-            totalTime += duration.count();
+            chrono::duration<double> elapsed = finish - begin;
+            accumulatedTime += elapsed.count();
+
+            delete[] values;
         }
 
-        double avgTime = totalTime / 10;
-        cout << "Size: " << n << ", Time: " << avgTime << " seconds" << endl;
+        double averageTime = accumulatedTime / 10.0;
+        cout << "Array Size: " << size << " --> Average Time: " << averageTime << " seconds" << endl;
 
-        n += 1000; // Increment n properly
+        size += 1000;
     }
 
     return 0;
 }
-
